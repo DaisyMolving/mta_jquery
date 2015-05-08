faeb916590fb52122f3f94f20aa9563d2cf6499b
Daisy Molving <daisymolving@gmail.com>
2015-05-08 20:27:23 +1200

    added event listeners to inputs for variable getUSerInput

--- MTA_jQuery.js
+++ MTA_jQuery.js
@@ -1,28 +1,46 @@
 // noprotect
+$(document).ready(function(){ 
+
+
+function randColor() {
+  return 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
+}
+
+var disco = $('.disco').each(function(index,element) {
+  setInterval(function(){
+     $(element).css('color', randColor());
+}, 20);
+});
+
+
+
+
 var mta = {
   'n': ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
   'l': ['8th-l', '6th', 'us', '3rd', '1st'],
   's': ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
 };
 
+
 function menu() {
-  var response = prompt('Welcome to MTA!!! \n(m)ta or (q)uit');
-  while(response !== 'q') {
     var userInput = getUserInput();
     var tripLength = calculateStops(userInput);
     alert('Your trip length is ' + tripLength + ' stops!');
   }
-}
+
 
 function getUserInput() {
-  var startTrain = prompt('What train do you want to get on at: \n' + Object.keys(mta).join(', ') + '?');
-  var firstStop = prompt('Which stop: ' + mta[startTrain].join(', '));
-  var stopTrain = prompt('What train do you want to get on at: \n' + Object.keys(mta).join(', ') + '?');
-  var lastStop = prompt('Which stop: ' + mta[stopTrain].join(', '));
+  var originStop = $('.result').on('click', function(){
+   ($('.start'));
+ });
+  var destinationStop = $('.result').on('click', function(){
+   ($('.finish'));
+   console.log(getUserInput);
   return {startTrain: startTrain, 
           firstStop: firstStop,
           stopTrain: stopTrain,
           lastStop: lastStop};
+        });
 }
 
 function calculateStops(userInput) {
@@ -33,7 +51,7 @@ function differentLines(userInput) {
   var intersection = mta[userInput.startTrain].filter(function(stop) {
       return mta[userInput.stopTrain].indexOf(stop) != -1;
     })[0];
-
+}
   var startTrainIndex = mta[userInput.startTrain].indexOf(userInput.firstStop);
   var startTrainIntersectionIndex = mta[userInput.startTrain].indexOf(intersection);
   var firstTripLength = Math.abs(startTrainIndex - startTrainIntersectionIndex);
@@ -42,7 +60,8 @@ function differentLines(userInput) {
   var stopTrainIntersectionIndex = mta[userInput.stopTrain].indexOf(intersection);
   var lastTripLength = Math.abs(stopTrainIndex - stopTrainIntersectionIndex);
 
-  return firstTripLength + lastTripLength;
+  $('.result').on('click', function(){ {
+  $('#balance1').html("$" + (firstTripLength + lastTripLength));
 }
 
 function sameLine(userInput) {
@@ -51,13 +70,5 @@ function sameLine(userInput) {
 
 menu();
 
-
-
-function randColor() {
-  return 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')'};
-var disco = $('h1').each(function(index,element) {
-  setInterval(function(){
-     $(element).css('color', randColor());
-}, 20)
-});
-          
+     });
+});          
